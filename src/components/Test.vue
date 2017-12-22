8efd7b1581fa1862e50e83742f62b607270825ee
valecalabrese <valent95@gmail.com>
2017-12-22 16:13:39 +0100

    My First Vue.js App

---
+++ src/components/Test.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="title"><br />
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.firstName}}</p>
+    <p v-if="showName">{{user.lastName}}</p>
+    <p v-else="showName">Nobody</p>
+    <ul>
+      <li v-for="item in items"> {{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Hello World!')">Say Greeting</button>
+    <br />
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+    <hr />
+    <label>First Name: </label><input type="text" v-model="user.firstName">
+      <br />
+      <label>Last Name: </label><input type="text" v-model="user.lastName">
+      <h3>{{fullName}}</h3>
+      <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    props: {
+      msg:{
+        type: String,
+        default: 'Foobar'
+      }
+    },
+    data() {
+    return {
+      title: 'Hello World',
+      user: {
+      firstName: 'Valentina',
+      lastName: 'Calabrese'
+    },
+    showName: true,
+    items: [
+      {title: 'Item One'},
+      {title: 'Item Two'},
+      {title: 'Item Three'}
+     ]
+    }
+  }, methods: {
+    greet: function(greeting){
+      alert(greeting);
+    },
+    pressKey: function(e){
+      console.log(e.target.value);
+    },
+    enterHit: function(){
+      console.log("You hit enter!");
+    }
+  },
+  computed: {
+    fullname: function(){
+      return this.user.firstName+ ' ' +this.user.lastName;
+    }
+   }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
